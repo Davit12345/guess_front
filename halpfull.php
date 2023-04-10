2cf857df39f1f94a63634ba86e41fd864fd1264a
Davit12345 <davitkarapet95@gmail.com>
2023-04-10 23:19:10 +0400

    add tt

---
+++ halpfull.php
@@ -0,0 +1,17 @@
+for listen word
+https://www.youtube.com/watch?v=T9XoKE8w0dI
+
+SpeechKit
+npm install cordova-plugin-nuance-speechkit
+npm install @ionic-native/speechkit
+
+
+Speech Recognition
+npm install cordova-plugin-speechrecognition
+npm install @ionic-native/speech-recognition
+
+for list show
+<ion-icon name="newspaper-outline"></ion-icon>
+
+
+C:\Python39\Scripts\;C:\Python39\;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\Program Files (x86)\Intel\iCLS Client\;C:\Program Files\Intel\iCLS Client\;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files\nodejs\;C:\ProgramData\chocolatey\bin;C:\Program Files\Java\jre1.8.0_271\bin;C:\Program Files\Java\jdk1.8.0_281\bin;C:\driver\platform-tools;
